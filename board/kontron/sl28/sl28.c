260f1e9521d16e87ed151aee10319cc514f05f3e
Michael Walle <michael.walle@kontron.com>
2019-03-25 16:50:52 +0100

    sl28: remove sl28.c from SPL
    Move common functions to common.c.
    
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- board/kontron/sl28/sl28.c
+++ board/kontron/sl28/sl28.c
@@ -71,12 +71,6 @@ int board_eth_init(bd_t *bis)
 	return pci_eth_init(bis);
 }
 
-int board_early_init_f(void)
-{
-	fsl_lsch3_early_init_f();
-	return 0;
-}
-
 void detail_board_ddr_info(void)
 {
 	puts("\nDDR    ");
