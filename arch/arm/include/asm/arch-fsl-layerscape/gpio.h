1923b08b822ef16780e1cec944843d387823f591
Michael Walle <michael.walle@kontron.com>
2019-04-08 17:22:37 +0200

    fsl-layerscape: add missing gpio.h
    Just include the parent imx include for now.
    
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

---
+++ arch/arm/include/asm/arch-fsl-layerscape/gpio.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2017 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __ASM_ARCH_FSL_LAYERSCAPE_GPIO_H
+#define __ASM_ARCH_FSL_LAYERSCAPE_GPIO_H
+
+#include <asm/mach-imx/gpio.h>
+
+#endif
