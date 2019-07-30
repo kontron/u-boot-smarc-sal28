d0429ffc232cfeb593346d6ed74c50c9243aca3d
Michael Walle <michael.walle@kontron.com>
2019-07-30 13:38:45 +0200

    sl28: set "bootsource" variable
    This variable indicates where the bootloader/spl/rcw was fetched from.
    
    Signed-off-by: Michael Walle <michael.walle@kontron.com>

--- board/kontron/sl28/sl28.c
+++ board/kontron/sl28/sl28.c
@@ -367,6 +367,7 @@ int board_late_init(void)
 	}
 	env_set_ulong("variant", sl28_variant());
 	env_set("rcw_filename", sl28_rcw_filename(buf, sizeof(buf)));
+	env_set_ulong("bootsource", sl28_boot_source());
 
 	sl28_set_prompt();
 
